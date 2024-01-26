cc4dbdf3028c3f8cbb2bf883fe3a4bb2cd0bedd2
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:13:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "uvc-student-centric-45586",
+  url: "https://uvc-student-centric-45586.botics.co",
+  api: "https://uvc-student-centric-45586.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
